5ddbac86095db0b80f65fad606687d9fe9389479
Albert Giménez Masclans <albertgimenez@ceroca.cat>
2024-09-19 19:31:23 +0200

    Colision de la nave

--- Assets/NouJugador.cs
+++ Assets/NouJugador.cs
@@ -6,11 +6,21 @@ public class NouJugador : MonoBehaviour
 {
     private float _vel;
 
+    private Vector2 minPantalla;
+    private Vector2 maxPantalla;
+
     // Start is called before the first frame update
     void Start()
     {
-
         _vel = 8;
+
+         minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+         maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        minPantalla.x = minPantalla.x + 0.75f;
+        maxPantalla.x = maxPantalla.x + 0.75f;
+        minPantalla.y = minPantalla.y + -0.75f;
+        maxPantalla.y = maxPantalla.y + -0.75f;
     }
 
     // Update is called once per frame
@@ -25,6 +35,9 @@ public class NouJugador : MonoBehaviour
         Vector2 Dir = new Vector2 (DirX, DirY).normalized;
         Vector2 NewPos = transform.position;
         NewPos = NewPos + Dir * _vel * Time.deltaTime;
+        NewPos.x = Mathf.Clamp(NewPos.x, minPantalla.x, maxPantalla.x);
+        NewPos.y = Mathf.Clamp(NewPos.y, minPantalla.y, maxPantalla.y);
         transform.position = NewPos;
+
     }
 }
